1109a3829d276d3357bc1717cd34dff68a77463f
Nguyễn Sĩ Liêm <63135340+ngslim@users.noreply.github.com>
2022-12-19 13:05:30 +0700

    add movement script

---
+++ The Knights/Assets/Script/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody2D rb;
+    Vector2 direction;
+    Vector2 faceDirection = Vector2.zero;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 temp = direction;
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+        if (direction == Vector2.zero && temp != Vector2.zero)
+        {
+            faceDirection = temp;
+            Debug.Log("Last face: " + faceDirection);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (direction != Vector2.zero)
+        {
+            rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+        }
+    }
+}
