c1afbb4d4683bfaf64d8f81da83c6fdffa85ac6a
LuanNguyen13421 <thanhluan13421@gmail.com>
2022-12-20 14:52:09 +0700

    Adjust player attack script

--- The Knights/Assets/Script/Player.cs
+++ The Knights/Assets/Script/Player.cs
@@ -4,44 +4,66 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float health = 3f;
+    public float health = 10.0f;
     public float attackSpeed = 0.5f;
     public float damage = 1f;
-
-    public float attackForce = 200f;
-    public float knockBack = 10f;
-
+    public float attackForce = 5.0f;
+    public float knockBack = 1.0f;
     public GameObject projectilePrefab;
+    public Animator animator;
 
     float attackCooldown;
-    bool isShootable = true;
+    bool isMeleeCombat = false;
+    //bool isShootable = true;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        attackCooldown -= Time.deltaTime;
-        if (attackCooldown <= 0)
+        // attackCooldown -= Time.deltaTime;
+        // if (attackCooldown <= 0)
+        // {
+        //     isShootable = true;
+        // }
+        if (attackCooldown > 0)
         {
-            isShootable = true;
+            attackCooldown -= Time.deltaTime;
         }
+
+        // Change attack range
+        
+
+        // Attack
         if(Input.GetButtonDown("Fire1"))
         {
-            if(isShootable)
+            if (isMeleeCombat)
             {
-                Shoot();
-                attackCooldown = attackSpeed;
-                isShootable = false;
+                if (attackCooldown <= 0)
+                {
+
+                }
+            }
+            else
+            {
+                if (attackCooldown <= 0)
+                {
+                    RangedAttack();
+                    attackCooldown = attackSpeed;
+                    //isShootable = false;
+                }
             }
         }
     }
 
-    void Shoot()
+    void RangedAttack()
     {
+        // Play animation
+        animator.SetTrigger("Attack");
+        // Create projectile
         GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
         projectile.GetComponent<Projectile>().SetDamage(damage);
         projectile.GetComponent<Projectile>().SetSource(gameObject.transform.tag);
@@ -49,6 +71,16 @@ public class Player : MonoBehaviour
         Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 source = new Vector2(transform.position.x, transform.position.y);
         projectile.GetComponent<Rigidbody2D>().AddForce((target - source).normalized * attackForce, ForceMode2D.Impulse);
+    }
+
+    void MeleeAttack()
+    {
+        // Play animation
+
+        // Detect enemy in attack range
+
+        // Damage enemy
+        
     }
 
     public void ModifyHealth(float amount)
