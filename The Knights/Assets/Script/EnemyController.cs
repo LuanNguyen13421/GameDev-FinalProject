9b7609e73dada4b264f78004d28a79243b40024e
Nguyễn Sĩ Liêm <63135340+ngslim@users.noreply.github.com>
2023-01-02 11:06:37 +0700

    add enemy roaming

--- The Knights/Assets/Script/EnemyController.cs
+++ The Knights/Assets/Script/EnemyController.cs
@@ -6,30 +6,59 @@ public class EnemyController : MonoBehaviour
 {
     Transform target;
     public float speed = 2f;
-    public float range = 10f;
-
+    public float lookRange = 10f;
+    public float roamingSpeed = 1f;
+    public float roamingRange = 10f;
+    public float timeBeforeNextRoam = 1f;
+     
+    float timeCounter = 0f;
+    Vector2 roamingTarget;
     bool isFollowingTarget = false;
+    bool isRoaming = false;
+
+    Vector2 rootPosition;
 
     private void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        rootPosition = transform.position;
     }
     private void OnDrawGizmos()
     {
         // Draw a yellow sphere at the transform's position
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, range);
+        Gizmos.DrawWireSphere(transform.position, lookRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, roamingRange);
     }
 
     private void Update()
     {
-        if (Vector2.Distance(transform.position, target.position) <= range)
+        if (Vector2.Distance(transform.position, target.position) <= lookRange)
         {
             isFollowingTarget = true;
+            isRoaming = false;
         }
         else
         {
+            if(isFollowingTarget)
+            {
+                rootPosition = transform.position;
+            }    
             isFollowingTarget = false;
+        }
+
+        if (timeCounter > 0)
+        {
+            timeCounter -= Time.deltaTime;
+        }
+        if (timeCounter <= 0 && !isRoaming)
+        {
+            isRoaming = true;
+            roamingTarget.x = Random.Range(rootPosition.x - roamingRange, rootPosition.x + roamingRange);
+            roamingTarget.y = Random.Range(rootPosition.y - roamingRange, rootPosition.y + roamingRange);
+            Debug.Log(roamingTarget);
         }
     }
 
@@ -39,6 +68,17 @@ public class EnemyController : MonoBehaviour
         if(isFollowingTarget)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+            //Debug.Log("Following target");
+        } else if (isRoaming)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, roamingTarget, roamingSpeed * Time.fixedDeltaTime);
+            //Debug.Log("Roaming");
+            Debug.Log(roamingTarget);
+            if (new Vector2(transform.position.x, transform.position.y) == roamingTarget)
+            {
+                isRoaming = false;
+                timeCounter = timeBeforeNextRoam;
+            }
         }
     }
 
