095dc910cff99bf0f577ce9a846d79ab3444e3d9
LuanNguyen13421 <thanhluan13421@gmail.com>
2023-01-03 15:26:45 +0700

    Adjust heath change

--- The Knights/Assets/Script/Player.cs
+++ The Knights/Assets/Script/Player.cs
@@ -91,7 +91,7 @@ public class Player : MonoBehaviour
 
     public void ModifyHealth(float amount)
     {
-        currentHealth = currentHealth + amount;
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
     }
     public void playSound(AudioClip clip)
     {
